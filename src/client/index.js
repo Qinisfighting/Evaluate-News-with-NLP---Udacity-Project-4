da2c4ad5ee80bd541f879ade401d3e8fee2b2c35
Qinisfighting <yq.qualmann@gmail.com>
2022-05-24 13:55:29 +0200

    use url loader to add logo

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,15 @@
 import { handleSubmit } from './js/formHandler'
 import { checkForURL } from './js/urlChecker'
-
-
+import logo from './img/logo.png'
 
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/form.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
+
+let homeImg = document.getElementById('logo');
+homeImg.src = logo;
 
 
 export {
--- webpack.prod.js
+++ webpack.prod.js
@@ -25,6 +25,16 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,  
+                use: [{
+                    loader: 'url-loader',
+                    options: { 
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    } 
+                }]
             }
         ]
     },
